d3e084eaacae89722d293ded373653aaacfdbdc6
zensh <txr1883@gmail.com>
2023-03-30 16:44:39 +0800

    improve redis client config; add redis client logs.

--- src/redis.rs
+++ src/redis.rs
@@ -1,7 +1,9 @@
+use async_trait::async_trait;
+use rustis::bb8::{CustomizeConnection, ErrorSink, Pool};
 use rustis::client::{Config, PooledClientManager, ServerConfig};
 use tokio::time::Duration;
 
-pub type RedisPool = rustis::bb8::Pool<PooledClientManager>;
+pub type RedisPool = Pool<PooledClientManager>;
 
 pub async fn new(cfg: super::conf::Redis) -> Result<RedisPool, rustis::Error> {
     let config = Config {
@@ -12,20 +14,50 @@ pub async fn new(cfg: super::conf::Redis) -> Result<RedisPool, rustis::Error> {
         username: Some(cfg.username).filter(|s| !s.is_empty()),
         password: Some(cfg.password).filter(|s| !s.is_empty()),
         connect_timeout: Duration::from_secs(3),
-        command_timeout: Duration::from_millis(300),
-        keep_alive: Some(Duration::from_secs(65)),
+        command_timeout: Duration::from_millis(100),
+        keep_alive: Some(Duration::from_secs(600)),
         ..Config::default()
     };
 
+    let max_size = if cfg.max_connections > 0 {
+        cfg.max_connections as u32
+    } else {
+        10
+    };
+    let min_idle = if max_size <= 10 { 1 } else { max_size / 10 };
+
     let manager = PooledClientManager::new(config).unwrap();
     RedisPool::builder()
-        .max_size(cfg.max_connections as u32)
-        .min_idle(Some(5))
+        .max_size(max_size)
+        .min_idle(Some(min_idle))
         .max_lifetime(None)
-        .idle_timeout(Some(Duration::from_secs(120)))
+        .idle_timeout(Some(Duration::from_secs(600)))
         .connection_timeout(Duration::from_secs(3))
+        .error_sink(Box::new(RedisMonitor {}))
+        .connection_customizer(Box::new(RedisMonitor {}))
         .build(manager)
         .await
+}
+
+#[derive(Debug, Clone, Copy)]
+struct RedisMonitor;
+
+impl<E: std::fmt::Display> ErrorSink<E> for RedisMonitor {
+    fn sink(&self, error: E) {
+        log::error!(target: "redis", "{}", error);
+    }
+
+    fn boxed_clone(&self) -> Box<dyn ErrorSink<E>> {
+        Box::new(*self)
+    }
+}
+
+#[async_trait]
+impl<C: Send + 'static, E: 'static> CustomizeConnection<C, E> for RedisMonitor {
+    async fn on_acquire(&self, _connection: &mut C) -> Result<(), E> {
+        log::info!(target: "redis", "connection acquired");
+        Ok(())
+    }
 }
 
 #[cfg(test)]
